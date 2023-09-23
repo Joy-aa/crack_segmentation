fe8e7f34b40b9e44f88148ebbb3f2ac6e9d95bf2
Joy-aa <1663607838@qq.com>
2023-09-23 09:32:13 +0000

    unet++

--- unet++/train.py
+++ unet++/train.py
@@ -48,7 +48,15 @@ def adjust_learning_rate(optimizer, epoch, lr):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
-def train(train_loader, valid_loader, model, criterion, optimizer, validation, args):
+def calc_loss(masks_pred, target_var, criterion, r=1):
+    masks_probs_flat = masks_pred.view(-1)
+    true_masks_flat  = target_var.view(-1)
+    
+    loss = r * criterion(masks_probs_flat, true_masks_flat)
+    loss += dice_loss(torch.sigmoid(masks_pred.squeeze(1)), target_var.squeeze(1).float(), multiclass=False)
+    return loss
+
+def train(dataset, model, criterion, optimizer, validation, args):
 
     # latest_model_path = find_latest_model_path(args.model_dir)
     # latest_model_path = os.path.join(*[args.model_dir, 'model_start.pt'])
@@ -77,12 +85,17 @@ def train(train_loader, valid_loader, model, criterion, optimizer, validation, a
         print(f'Started training model from epoch {epoch}')
     else:
         print('Started training model from epoch 0')
-        epoch = 0
+        epoch = 1
         min_val_los = 9999
 
     valid_losses = []
     total_iter = 0
     for epoch in range(epoch, args.n_epoch + 1):
+
+        train_size = int(len(dataset)*0.9)
+        train_dataset, valid_dataset = random_split(dataset, [train_size, len(dataset) - train_size])
+        train_loader = torch.utils.data.DataLoader(train_dataset, args.batch_size, shuffle=True, pin_memory=torch.cuda.is_available(), num_workers=4)
+        valid_loader = torch.utils.data.DataLoader(valid_dataset, 1, shuffle=False, pin_memory=torch.cuda.is_available(), num_workers=4)
 
         adjust_learning_rate(optimizer, epoch, args.lr)
 
@@ -90,7 +103,6 @@ def train(train_loader, valid_loader, model, criterion, optimizer, validation, a
         tq.set_description(f'Epoch {epoch}')
 
         losses = AverageMeter()
-        report_interval = 100
         model.train()
         for i, (input, target) in enumerate(train_loader):
             input_var  = Variable(input).cuda()
@@ -98,14 +110,10 @@ def train(train_loader, valid_loader, model, criterion, optimizer, validation, a
 
             masks_pred = model(input_var)
 
-            masks_probs_flat = masks_pred.view(-1)
-            true_masks_flat  = target_var.view(-1)
-
-            loss = criterion(masks_probs_flat, true_masks_flat)
-            loss += dice_loss(F.sigmoid(masks_pred.squeeze(1)), target_var.squeeze(1).float(), multiclass=False)
+            loss = calc_loss(masks_pred, target_var, criterion, 10)
             losses.update(loss)
 
-            if (i+total_iter) % report_interval == 0:
+            if (i+total_iter) % args.print_freq == 0:
                 logger.log_scalar('train/lr', optimizer.param_groups[0]['lr'], i+total_iter)
                 logger.log_scalar('train/loss', losses.avg, i+total_iter)
 
@@ -126,14 +134,24 @@ def train(train_loader, valid_loader, model, criterion, optimizer, validation, a
         tq.close()
 
         #save the model of the current epoch
-        if np.mod(epoch+1, 5) == 0:
+        if np.mod(epoch, 5) == 0:
             # epoch_model_path = os.path.join(*[args.model_dir, f'model_epoch_{epoch}.pt'])
             torch.save({
                 'model': model.state_dict(),
                 'epoch': epoch,
                 'valid_loss': valid_loss,
                 'train_loss': losses.avg
-            }, f'checkpoints/Unet++_{epoch+1}.pth')
+            }, f'checkpoints/Unet++_{epoch}.pth')
+
+        if valid_loss < min_val_los:
+            min_val_los = valid_loss
+
+            torch.save({
+                'model': model.state_dict(),
+                'epoch': epoch,
+                'valid_loss': valid_loss,
+                'train_loss': losses.avg
+            }, best_model_path)
 
 def validate(model, val_loader, criterion):
     losses = AverageMeter()
@@ -147,9 +165,8 @@ def validate(model, val_loader, criterion):
             target_var = Variable(target).cuda()
 
             output = model(input_var)
-            loss = criterion(output, target_var)
-            loss += dice_loss(F.sigmoid(output.squeeze(1)), target_var.squeeze(1).float(), multiclass=False)
-            losses.update(loss.item(), input_var.size(0))
+            loss = calc_loss(output, target_var, criterion, 10)
+            losses.update(loss.item())
 
     return {'valid_loss': losses.avg}
 
@@ -197,10 +214,10 @@ def predict(test_loader, model, latest_model_path):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
-    parser.add_argument('--n_epoch', default=20, type=int, metavar='N', help='number of total epochs to run')
+    parser.add_argument('--n_epoch', default=50, type=int, metavar='N', help='number of total epochs to run')
     parser.add_argument('--lr', default=0.001, type=float, metavar='LR', help='initial learning rate')
     parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum')
-    parser.add_argument('--print_freq', default=20, type=int, metavar='N', help='print frequency (default: 10)')
+    parser.add_argument('--print_freq', default=100, type=int, metavar='N', help='print frequency (default: 10)')
     parser.add_argument('--weight_decay', default=1e-4, type=float, metavar='W', help='weight decay (default: 1e-4)')
     parser.add_argument('--batch_size',  default=8, type=int,  help='weight decay (default: 1e-4)')
     parser.add_argument('--num_workers', default=4, type=int, help='output dataset directory')
@@ -210,61 +227,76 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     os.makedirs(args.model_dir, exist_ok=True)
-
-    TRAIN_IMG  = os.path.join(args.data_dir, 'imgs')
-    TRAIN_MASK = os.path.join(args.data_dir, 'masks')
-
-
-    train_img_names  = [path.name for path in Path(TRAIN_IMG).glob('*.png')]
+# 第一阶段训练
+    TRAIN_IMG  = os.path.join(args.data_dir, 'image')
+    TRAIN_MASK = os.path.join(args.data_dir, 'label')
+    train_img_names  = [path.name for path in Path(TRAIN_IMG).glob('*.jpg')]
     train_mask_names = [path.name for path in Path(TRAIN_MASK).glob('*.png')]
-    # train_img_names = train_img_names[:len(train_img_names)*0.1]
-    # train_mask_names = train_mask_names[:len(train_mask_names)*0.1]
     print(f'total train images = {len(train_img_names)}')
 
     channel_means = [0.485, 0.456, 0.406]
     channel_stds  = [0.229, 0.224, 0.225]
     train_tfms = transforms.Compose([transforms.ToTensor(),
                                      transforms.Normalize(channel_means, channel_stds)])
-
     val_tfms = transforms.Compose([transforms.ToTensor(),
                                    transforms.Normalize(channel_means, channel_stds)])
-
     mask_tfms = transforms.Compose([transforms.ToTensor()])
 
     train_dataset = ImgDataSet(img_dir=TRAIN_IMG, img_fnames=train_img_names, img_transform=train_tfms, mask_dir=TRAIN_MASK, mask_fnames=train_mask_names, mask_transform=mask_tfms)
-    _dataset, test_dataset = random_split(train_dataset, [0.9, 0.1],torch.Generator().manual_seed(42))
-    train_dataset, valid_dataset = random_split(_dataset, [0.9, 0.1],torch.Generator().manual_seed(42))
-    train_loader = torch.utils.data.DataLoader(train_dataset, args.batch_size, shuffle=True, pin_memory=torch.cuda.is_available(), num_workers=4)
-    val_loader = torch.utils.data.DataLoader(valid_dataset, 1, shuffle=False, pin_memory=torch.cuda.is_available(), num_workers=4)
-    test_loader = torch.utils.data.DataLoader(test_dataset, 1, shuffle=False, pin_memory=torch.cuda.is_available(), num_workers=4)
+
+# 第二阶段训练
+    # TRAIN_IMG  = os.path.join(args.data_dir, 'imgs')
+    # TRAIN_MASK = os.path.join(args.data_dir, 'masks')
+
+
+    # train_img_names  = [path.name for path in Path(TRAIN_IMG).glob('*.jpg')]
+    # train_mask_names = [path.name for path in Path(TRAIN_MASK).glob('*.png')]
+    # print(f'total train images = {len(train_img_names)}')
+
+    # channel_means = [0.485, 0.456, 0.406]
+    # channel_stds  = [0.229, 0.224, 0.225]
+    # train_tfms = transforms.Compose([transforms.ToTensor(),
+    #                                  transforms.Normalize(channel_means, channel_stds)])
+
+    # val_tfms = transforms.Compose([transforms.ToTensor(),
+    #                                transforms.Normalize(channel_means, channel_stds)])
+
+    # mask_tfms = transforms.Compose([transforms.ToTensor()])
+
+    # train_dataset = ImgDataSet(img_dir=TRAIN_IMG, img_fnames=train_img_names, img_transform=train_tfms, mask_dir=TRAIN_MASK, mask_fnames=train_mask_names, mask_transform=mask_tfms)
+    # _dataset, test_dataset = random_split(train_dataset, [0.9, 0.1],torch.Generator().manual_seed(42))
+    # train_dataset, valid_dataset = random_split(_dataset, [0.9, 0.1],torch.Generator().manual_seed(42))
+    # train_loader = torch.utils.data.DataLoader(train_dataset, args.batch_size, shuffle=True, pin_memory=torch.cuda.is_available(), num_workers=4)
+    # val_loader = torch.utils.data.DataLoader(valid_dataset, 1, shuffle=False, pin_memory=torch.cuda.is_available(), num_workers=4)
+    # test_loader = torch.utils.data.DataLoader(test_dataset, 1, shuffle=False, pin_memory=torch.cuda.is_available(), num_workers=4)
 
 
     long_id = '%s_%s' % (str(args.lr), datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S'))
     logger = BoardLogger(long_id)
 
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
     device = torch.device("cuda")
     # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     num_gpu = torch.cuda.device_count()
     model = UnetPlusPlus(num_classes=1)
     model = torch.nn.DataParallel(model, device_ids=range(num_gpu))
     model.to(device)
-    # lossf = nn.BCEWithLogitsLoss()
+    criterion = nn.BCEWithLogitsLoss()
     #选用adam优化器来训练
     optimizer = torch.optim.SGD(model.parameters(), args.lr,
                                 momentum=args.momentum,
                                 weight_decay=args.weight_decay)
-    criterion = BinaryFocalLoss().to(device)
 
-    # train(train_loader, val_loader, model, criterion, optimizer, validate, args)
+    train(train_dataset, model, criterion, optimizer, validate, args)
 
-    latest_model_path = '/home/wj/local/crack_segmentation/unet++/checkpoints/Unet++_20.pth'
-    state = torch.load(latest_model_path)
-    model.load_state_dict(state['model'])
-    # weights = state['model']
-    # weights_dict = {}
-    # for k, v in weights.items():
-    #     new_k = k.replace('module.', '') if 'module' in k else k
-    #     weights_dict[new_k] = v
-    # model.load_state_dict(weights_dict)
-    predict(test_loader, model, latest_model_path)
+    # latest_model_path = '/home/wj/local/crack_segmentation/unet++/checkpoints/Unet++_20.pth'
+    # state = torch.load(latest_model_path)
+    # model.load_state_dict(state['model'])
+    # # weights = state['model']
+    # # weights_dict = {}
+    # # for k, v in weights.items():
+    # #     new_k = k.replace('module.', '') if 'module' in k else k
+    # #     weights_dict[new_k] = v
+    # # model.load_state_dict(weights_dict)
+    # predict(test_loader, model, latest_model_path)
     
