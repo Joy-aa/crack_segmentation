6ed7db641c2e5b28d43b25f40fa4bfd7a0591a60
Joy-aa <1663607838@qq.com>
2023-10-21 22:31:20 +0000

    add invest

--- unet/inference.py
+++ unet/inference.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     parser.add_argument('--img_dir',type=str, default='../images', help='input dataset directory')
     parser.add_argument('--model_path', type=str, default='./checkpoints/model_best.pt', help='trained model path')
     parser.add_argument('--model_type', type=str, default='vgg16', choices=['vgg16', 'vgg16V2', 'resnet101', 'resnet34'])
-    parser.add_argument('--out_pred_dir', type=str, default='./result_img', required=False,  help='prediction output dir')
+    parser.add_argument('--out_pred_dir', type=str, default='./result_imgV2', required=False,  help='prediction output dir')
     parser.add_argument('--type', type=str, default='out' , choices=['out', 'metric'])
     args = parser.parse_args()
 
@@ -88,6 +88,7 @@ if __name__ == '__main__':
     channel_means = [0.485, 0.456, 0.406]
     channel_stds  = [0.229, 0.224, 0.225]
 
+    print()
     paths = [path for path in Path(DIR_IMG).glob('*.*')]
     metrics=[]
     for path in tqdm(paths):
