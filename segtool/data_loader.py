52c10c9bfbd834a764d51955807fae83b6950b32
Joy-aa <1663607838@qq.com>
2023-12-06 18:10:29 +0000

    update loss and dataloader

--- segtool/data_loader.py
+++ segtool/data_loader.py
@@ -4,6 +4,9 @@ from torch.utils.data import DataLoader, Dataset
 import random
 from PIL import Image
 import matplotlib.pyplot as plt
+import torch
+from segtool import edge_utils
+import cv2
 
 class ImgDataSet(Dataset):
 
@@ -91,6 +94,59 @@ class ImgDataSetJoint(Dataset):
             mask = self.mask_transform(mask)
 
         return img, mask #torch.from_numpy(np.array(mask, dtype=np.int64))
+
+    def __len__(self):
+        return len(self.img_fnames)
+    
+
+class CrackDataSet(Dataset):
+
+    def __init__(self, img_dir, img_fnames, img_transform, mask_dir, mask_fnames, mask_transform):
+        self.img_dir = img_dir
+        self.img_fnames = img_fnames
+        self.img_transform = img_transform
+
+        self.mask_dir = mask_dir
+        self.mask_fnames = mask_fnames
+        self.mask_transform = mask_transform
+
+        self.seed = np.random.randint(2147483647)
+
+    def __getitem__(self, i):
+        fname = self.img_fnames[i]
+        fpath = os.path.join(self.img_dir, fname)
+        img = Image.open(fpath)
+        if self.img_transform is not None:
+            random.seed(self.seed)
+            img = self.img_transform(img)
+
+        mname = self.mask_fnames[i]
+        # mname = fname.split('.')[0] + '.png'
+        mpath = os.path.join(self.mask_dir, mname)
+        mask = Image.open(mpath).convert('L') 
+        mask_copy = np.array(mask)
+        mask_copy[mask_copy < 127 ] = 0
+        mask_copy[mask_copy > 0 ] = 255
+        # mask_copy[mask_copy == 38 ] = 0
+        # mask_copy[mask_copy == 75 ] = 255
+        mask = Image.fromarray(mask_copy.astype(np.uint8))
+        
+        if self.mask_transform is not None:
+            mask = self.mask_transform(mask)
+        
+        mask = mask.squeeze(0)
+        
+        _edgemap = (mask.numpy())
+        # print(_edgemap.shape)
+        _edgemap = edge_utils.mask_to_onehot(_edgemap, 1)
+        # print(_edgemap.shape)
+
+        _edgemap = edge_utils.onehot_to_binary_edges(_edgemap, 1, 1)
+
+        edgemap = torch.from_numpy(_edgemap).float()
+        # print(edgemap)
+
+        return img, mask, edgemap
 
     def __len__(self):
         return len(self.img_fnames)
